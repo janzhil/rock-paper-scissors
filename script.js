1b97677b41e17ef5250da4b557f8da2f5e4fe564
Jan <jan.ziljak@gmail.com>
2022-06-16 10:58:48 +0200

    created a 5 round game function and added a score counter

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let computerChoice;
+let playerScore = 0;
+let computerScore = 0;
+let tieRounds = 0;
 
 function computerPlay(){
     const randomNumber = Math.floor(Math.random()* 3) + 1;
@@ -12,38 +16,66 @@ function computerPlay(){
 }
 
 
-let computerChoice;
 
 function playRound(){
-    playerSelection = prompt("").toLowerCase();
+    playerSelection = prompt("Rock, Paper or Scissors?","").toLowerCase();
     computerChoice = computerPlay(); 
+
     if(playerSelection == "rock" && computerChoice == "scissors"){
-        alert("Player wins this round!");
+        alert("Player wins!");
         console.log(computerChoice);
         console.log(playerSelection);
+        playerScore += 1;
+        return playerScore;
     } else if(playerSelection == "rock" && computerChoice == "rock"){
         alert("It's a tie!");
         console.log(computerChoice);
         console.log(playerSelection);
+        tieRounds += 1;
+        return tieRounds;
     }else if(playerSelection == "scissors" && computerChoice == "paper"){
-        alert("Player wins this round!");
+        alert("Player wins!");
         console.log(computerChoice);
         console.log(playerSelection);
+        playerScore += 1;
+        return playerScore;
     }else if(playerSelection == "scissors" && computerChoice == "scissors"){
         alert("It's a tie!");
         console.log(computerChoice);
         console.log(playerSelection);
+        tieRounds += 1;
+        return tieRounds;
     }else if(playerSelection == "paper" && computerChoice == "rock"){
-    alert("Player wins this round!");
-    console.log(computerChoice);
-    console.log(playerSelection);
+        alert("Player wins!");
+        console.log(computerChoice);
+        console.log(playerSelection);
+        playerScore += 1;
+        return playerScore;
     }else if(playerSelection == "paper" && computerChoice == "paper"){
-    alert("It's a tie!");
-    console.log(computerChoice);
-    console.log(playerSelection);
+        alert("It's a tie!");
+        console.log(computerChoice);
+        console.log(playerSelection);
+        tieRounds += 1;
+        return tieRounds;
     } else{
-    alert("Computer wins this round!");
-    console.log(computerChoice);
-    console.log(playerSelection);
+        alert("Computer wins!");
+        console.log(computerChoice);
+        console.log(playerSelection);
+        computerScore +=1
+        return computerScore;
+    }
+}
+
+
+function playGame(){   
+    for( let i = 0; i < 5; i++) {
+        playRound();
+    }
+    if (playerScore > computerScore){
+        alert("Player wins the game!");
+    }else if( playerScore < computerScore){
+        alert("Computer wins the game!")
+    }else{
+        alert("It's a tie!")
     }
 }
